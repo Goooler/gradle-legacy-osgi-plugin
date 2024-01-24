b941a218a7a89beb207dec0033d7aec22872a017
Goooler <wangzongler@gmail.com>
2024-01-25 01:12:49 +0800

    Try out io.github.gradle-nexus.publish-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     id("java-library")
     id("groovy")
     id("com.gradle.plugin-publish") version "1.2.1"
-    id("com.vanniktech.maven.publish") version "0.26.0"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-1"
+    signing
 }
 
-group = providers.gradleProperty("GROUP").orNull.orEmpty()
-version = providers.gradleProperty("VERSION_NAME").orNull.orEmpty()
+group = providers.gradleProperty("GROUP").get()
+version = providers.gradleProperty("VERSION_NAME").get()
 
 java {
     toolchain.languageVersion = JavaLanguageVersion.of(8)
@@ -39,6 +38,66 @@ gradlePlugin {
             tags = listOf("legacy", "osgi")
         }
     }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl = uri("https://s01.oss.sonatype.org/service/local/")
+            snapshotRepositoryUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            username = providers.gradleProperty("mavenCentralUsername")
+            password = providers.gradleProperty("mavenCentralPassword")
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = providers.gradleProperty("POM_ARTIFACT_ID").get()
+            from(components["java"])
+
+            pom {
+                name = providers.gradleProperty("POM_NAME")
+                description = providers.gradleProperty("POM_DESCRIPTION")
+                url = providers.gradleProperty("POM_URL")
+                licenses {
+                    license {
+                        name = providers.gradleProperty("POM_LICENSE_NAME")
+                        url = providers.gradleProperty("POM_LICENSE_URL")
+                    }
+                }
+                developers {
+                    developer {
+                        id = providers.gradleProperty("POM_DEVELOPER_ID")
+                        name = providers.gradleProperty("POM_DEVELOPER_NAME")
+                        url = providers.gradleProperty("POM_DEVELOPER_URL")
+                    }
+                }
+                scm {
+                    connection = providers.gradleProperty("POM_SCM_CONNECTION")
+                    developerConnection = providers.gradleProperty("POM_SCM_DEV_CONNECTION")
+                    url = providers.gradleProperty("POM_SCM_URL")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+    isRequired = !version.toString().endsWith("-SNAPSHOT")
+}
+
+// TODO: workaround for https://github.com/gradle-nexus/publish-plugin/issues/208.
+tasks.withType<PublishToMavenRepository>().configureEach {
+    dependsOn(tasks.withType<Sign>())
+}
+
+tasks.matching {
+    it.name.matches(Regex("^publish.*ToMavenLocal\$"))
+}.configureEach {
+    dependsOn(tasks.withType<Sign>())
 }
 
 tasks.publishPlugins {
